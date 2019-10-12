0e8fd0bab539fbce652feff7c371db2f6b8c1e27
Carlos <lozgarrido@gmail.com>
2019-10-12 12:42:27 +0200

    Countries validator moved to .py

--- import_data.py
+++ import_data.py
@@ -1,33 +1,6 @@
 import requests
 import pandas as pd
 
-def get_country_code(country, code_len=3):
-    '''Obtains ISO 3166 code given a country.
-
-    Args:
-        country: country name
-        code_len: alpha len of code (2 or 3, default 3)
-    '''
-
-    # API calling
-    query = 'https://restcountries.eu/rest/v2/name' + country
-    country_info = requests.get(query).json()[0]
-
-    # Extract country code from json
-    country_code_key = f'alpha{str(code_len)}Code'
-    country_code = country_info.get(country_code_key)
-
-    return country_code
-
-def check_country_code(country_code):
-    '''Returns a boolean if a country code exists.'''
-
-    # API calling
-    query = 'https://restcountries.eu/rest/v2/alpha/' + country_code
-
-    # Check if the response is valid
-    return requests.get(query).ok
-
 def get_annual_asylum_stats(year, origin_country=None, asylum_country=None):
     '''Returns a dicionary with asylum seekers stats in given year.
 
